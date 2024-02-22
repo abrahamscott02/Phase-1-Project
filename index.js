1fae502d14944730b560a3636ce50c81d8895281
Abraham Scott <abrahamscott02@gmail.com>
2024-02-22 06:19:14 -0700

    update

---
+++ index.js
@@ -0,0 +1,4 @@
+const amountInput = document.getElementById('amountInput');
+const fromCurrency = document.getElementById('fromCurrency');
+const toCurrency = document.getElementById('toCurrency');
+
