48a784a51d6fb4030f2fd4171aac24743c611513
Abraham Scott <abrahamscott02@gmail.com>
2024-02-23 17:47:25 -0700

    Update

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 const amountInput = document.getElementById('amountInput');
 const fromCurrency = document.getElementById('fromCurrency');
 const toCurrency = document.getElementById('toCurrency');
+const converterBtn = document.getElementById('converterBtn');
+const resultsDiv = document.getElementById('results');
 
+function fetchCurrencyData(typeOfCurrency){
+    const apiUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${typeOfCurrency}.json`;
+ 
+    fetch(apiUrl)
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error('Issue with the Networks response');
+        } else
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+}
+})
